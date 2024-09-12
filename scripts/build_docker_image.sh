bda1f2c0179f67b9b7ae8e493c945efc0b7a0508
Alessandro Bolletta <alexpacio91@gmail.com>
2024-09-12 18:02:26 +0200

    feat: implemented helm chart

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker build -t boilerplate/backend-api:latest -f ./backend-api/Dockerfile backend-api/
+docker build -t localhost:32000/backend-api:latest -f ./backend-api/Dockerfile backend-api/
+docker push localhost:32000/backend-api:latest
+docker build -t localhost:32000/aws-bootstrapper:latest -f ./aws-bootstrapper/Dockerfile aws-bootstrapper/
+docker push localhost:32000/aws-bootstrapper:latest
